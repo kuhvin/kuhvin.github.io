4b83d18975ebef92f2c20aa63a1f026832fad60f
kuhvin <kuhvin@student.ubc.ca>
2024-03-29 22:51:28 -0700

    Debugging

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // App.js
 import React from 'react';
 import Profile from './Profile';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
     return (
         <div className="App">
             <Profile />
+            <Analytics id="ANALYTICS_ID" />
         </div>
     );
 }
