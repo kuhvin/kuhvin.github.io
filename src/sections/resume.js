22988f57fbc4611beaaf6b08f9b1963dccfff3de
kuhvin <kuhvin@student.ubc.ca>
2024-04-06 22:32:56 -0700

    updated site resume

--- src/sections/resume.js
+++ src/sections/resume.js
@@ -15,8 +15,8 @@ class Resume extends React.Component {
       <h5 style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}> 
             <a className="custom-link" href="mailto:kevinli5090@gmail.com">kevinli5090@gmail.com</a> | 
             780-566-5090 | 
-            <a className="custom-link" href="https://github.com/kuhvin" target="_blank" rel="noopener noreferrer">https://github.com/kuhvin</a> | 
-            <a className="custom-link" href="https://www.linkedin.com/in/kevinli5090/" target="_blank" rel="noopener noreferrer">https://www.linkedin.com/in/kevinli5090/</a>
+            <a className="custom-link" href="https://www.linkedin.com/in/kevinli5090/" target="_blank" rel="noopener noreferrer">https://www.linkedin.com/in/kevinli5090/</a> | 
+            <a className="custom-link" href="kuhvin.ca" target="_blank" rel="noopener noreferrer">kuhvin.ca</a>
       </h5>
       <div style={{textAlign: 'left', margin: '-50px'}}>
           <h2>EDUCATION</h2>
@@ -25,40 +25,58 @@ class Resume extends React.Component {
               <p><b>University of British Columbia - Vancouver, BC</b></p>
               <p><b>Expected Graduation: May 2025</b></p>
           </div>
-          <p style={{marginTop: '0', marginBottom: '30px'}}>Bachelor of Science - Mathematics</p>
+          <p style={{marginTop: '0', marginBottom: '30px'}}>Bachelor of Science - Mathematics
+          <ul style = {{marginBottom: '30px'}}> 
+            <li> <strong>Relevant Coursework:</strong> Data Structures, Algorithms, Relational Databases, Software Engineering</li>
+          </ul>
+          </p>
           <h2>TECHNICAL SKILLS</h2>
           <hr/>
-          <ul style = {{marginBottom: '30px'}}>
-            <li><strong>Languages:</strong> Java, Python, SQL, C++, C, Javascript, CSS, HTML, Latex, Assemly, R, PHP</li>
-            <li><strong>Tools:</strong> Git, JSON, R Studio, NLTK, MySQL, Swing, XML, Java Card, JSON, React, Node.js, Excel, Sharepoint, Flow Automate, VS Code, IntelliJ, Pycharm</li>
+          <ul style = {{marginBottom: '30px'}}> 
+            <li><strong>Languages:</strong> Java, JavaScript, Python, SQL, C++, C, HTML, JSON, PHP</li>
+            <li><strong>Tools:</strong> React, Express, Node, Git, Slack, MySQL, Junit, Excel, Microsoft PowerApps </li>
           </ul>
-          <h2>WORK EXPERIENCE</h2>
+          <h2>WORK/VOLUNTEER EXPERIENCE</h2>
           <hr/>
           <div style={{display: 'flex', justifyContent: 'space-between'}}>
               <p><b>Bantrel - Project Controls Intern • Edmonton, AB</b></p>
               <p><b>May 2023 - August 2023</b></p>
           </div>
           <ul style = {{marginTop: '0'}}>
-            <li>Co-led the design and digitization of essential company files to a more central, online infrastructure using SharePoint </li>
-            <li>Completed numerous queries in SQL and Excel to streamline employee interaction with network drives and cloud databases </li>
-            <li>Restructured outdated Firefox and Microsoft Azure databases to a custom Bantrel SQL database </li>
-            <li>Created multiple user-friendly templates using Microsoft Power Apps and Flow Automate for file creation to streamline workflow and communication between engineering departments </li>
+            <li>Co-led the design and digitization of essential company files to a more central, online infrastructure surrounding Microsoft SharePoint </li>
+            <li>Completed comprehensive queries in SQL and Excel to streamline employee interaction with company network drive and cloud database, allowing for more seamless configuration for historic and future file storage </li>
+            <li>Restructured outdated Firefox and Microsoft Azure databases to a new company DBMS </li>
+            <li>Created multiple user-friendly templates using Microsoft Power Apps and Flow Automate for file creation to improve workflow and communication between engineering departments </li>
           </ul>
           <div style={{display: 'flex', justifyContent: 'space-between', }}>
               <p style = {{marginTop: '0'}}><b>Western Mechatronics - Business Manager • Calgary, AB</b></p>
               <p style = {{marginTop: '0'}}><b>July 2022 - Present</b></p>
           </div>
           <ul style = {{marginTop: '0', marginBottom: '30px'}}>
-            <li>Manager for application of grants and other subsidiaries totalling over $20,000 </li>
-            <li>Secured a partnership with Google through administering their Girl Powered Event </li>
-            <li>Mentor for students grades 5 to 12 to use VexCode to program the main processor for various mechatronic projects </li>
-            <li>Collaborated on a 12-person programming team developing a tournament software for a national competition </li>
+            <li>Managed the application of grants and other subsidiaries totalling over $20,000 </li>
+            <li>Active mentor for students grades 5 to 12 learning robotics, introducing key mechanical principles and fundamental programming processes  </li>
+            <li>Key volunteer at national robotics events, overseeing tournament progress and providing technical support in all areas</li>
           </ul>
           <h2>PROJECT EXPERIENCE</h2>
           <hr/>
           <div style={{display: 'flex', justifyContent: 'space-between', }}>
+              <p style = {{marginTop: '0'}}><b>Furever Friends - Class Project</b></p>
+          </div>
+          <ul style = {{marginTop: '0'}}>
+            <li>Developed a full-stack database application to help adopters find pets using MySQL, PHP, Express and JavaScript </li>
+            <li>Created numerous types of queries to match application demands, including real-time projection, aggregation and division</li>
+            <li>Implemented input sanitization by parameterizing MySQL user inputs to prevent injection</li>
+          </ul>
+          <div style={{display: 'flex', justifyContent: 'space-between', }}>
+              <p style = {{marginTop: '0'}}><b>Personal Website - Personal Project</b></p>
+          </div>
+          <ul style = {{marginTop: '0'}}>
+            <li>Created an online portfolio website using the React environment</li>
+            <li>Designed and implemented a minimalistic design and smooth user experience with HTML and CSS animations</li>
+            <li>Wrote website functionality using JavaScript to help navigate the entirety of the site</li>
+          </ul>
+          <div style={{display: 'flex', justifyContent: 'space-between', }}>
               <p style = {{marginTop: '0'}}><b>Volunteer Hours Tracker - Personal Project</b></p>
-              <p style = {{marginTop: '0'}}><b>April 2023 - June 2023</b></p>
           </div>
           <ul style = {{marginTop: '0'}}>
             <li>Developed a smartcard reader application in Java to help a non-profit organization track volunteer hours</li>
@@ -66,21 +84,12 @@ class Resume extends React.Component {
             <li>Implemented the Java Card applet and framework on the smart card, enabling interaction with the card terminal using ISO-7816 APDU commands</li>
           </ul>
           <div style={{display: 'flex', justifyContent: 'space-between', }}>
-              <p style = {{marginTop: '0'}}><b>Media Tracker - Personal Project</b></p>
-              <p style = {{marginTop: '0'}}><b>October 2022 - November 2022</b></p>
+              <p style = {{marginTop: '0'}}><b>Media Tracker - Class Project</b></p>
           </div>
           <ul style = {{marginTop: '0'}}>
             <li>Designed a user interface for a movie watchlist application using Java Swing</li>
             <li>Used JSON Reader and Writer to create temporary databases on local devices</li>
             <li>Tested implementation and exception handling using the JUnit library </li>
-          </ul>
-          <div style={{display: 'flex', justifyContent: 'space-between', }}>
-              <p style = {{marginTop: '0'}}><b>Junior Achievement - VP IT/Fiannce </b></p>
-              <p style = {{marginTop: '0'}}><b>July 2022 - Present</b></p>
-          </div>
-          <ul style = {{marginTop: '0', marginBottom: '30px'}}>
-            <li>Maintained and managed all income and expenses for a student-led small business</li>
-            <li>Led a four-person team to design the company website and social media pages using HTML</li>
           </ul>
           </div>
       </div>
