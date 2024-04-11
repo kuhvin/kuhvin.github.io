b81ae88ab22485d5ab80a4e925626f9c5a5fc9cf
kuhvin <kuhvin@student.ubc.ca>
2024-04-10 21:24:08 -0700

    "tweak"

--- src/sections/about.js
+++ src/sections/about.js
@@ -8,7 +8,7 @@ class About extends React.Component {
         <h2>Hello</h2>
         <p>I'm a 3rd-year mathematics student at the University of British Columbia.<br />I'm interested in machine learning, algorthims, and software development.
         </p>
-        <p>I'm an aspiring software engineer interested in all aspects of development. 
+        <p>I'm an a software engineer interested in all aspects of development. 
           <br/> So far, I've been lucky enough to be a project controls intern working on back-end
            <br /> optimization, and a
             volunteer to teach robotics to high school students. I'm always 
