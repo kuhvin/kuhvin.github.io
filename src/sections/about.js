c1eceec014c42478bd7274010d1c901e19ba4c54
kuhvin <firerage211@gmail.com>
2024-04-30 18:25:01 -0600

    made minor tweaks

--- src/sections/about.js
+++ src/sections/about.js
@@ -8,7 +8,7 @@ class About extends React.Component {
         <h2>Hello</h2>
         <p>I'm a 3rd-year mathematics student at the University of British Columbia.<br />I'm interested in machine learning, algorthims, and  all aspects of software development.
         </p>
-        <p>I'm currently a software engineer intern at ZiyuTec, where I'm<br />creating a central project management application from end-to-end. <br />
+        <p>I'm currently a software engineer intern at ZiyuTec, where I'm<br />creating a central project management application. <br />
           <br/> So far, I've been lucky enough to be a project controls intern working on back-end
            <br /> optimization, and a
             volunteer to teach robotics to high school students. I'm always 
